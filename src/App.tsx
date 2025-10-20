f26fc25175751dc2f08523e504232e51457562e7
Alejandro Patiño García <alexpatino92@yahoo.com>
2025-10-20 09:03:36 +0200

    feat: integrate ThemeProvider and ModeToggle for theme management

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
   let name: string = "Click Me";
@@ -10,6 +12,9 @@ function App() {
 
   return (
     <>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <ModeToggle />
+      </ThemeProvider>
       <div className="w-screen h-screen justify-center items-center flex flex-col gap-4">
         <div className="flex gap-2">
           <img src={viteLogo} className="logo" alt="Vite logo" />
